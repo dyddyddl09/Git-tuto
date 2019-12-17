b34534121e420aebcea5026adb99a8b68a27793d
YongHo <YongHo@DESKTOP-4O562D1>
2019-12-17 17:51:38 +0900

    종료
    냉요엉이런저런것을해사다당
    나너아ㅓㄹ
    1번뭐햇ㅇ]고

--- Git-tuto/src/org/homegit/MainClass.java
+++ Git-tuto/src/org/homegit/MainClass.java
@@ -4,7 +4,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		System.out.println("작업자 용호 쏼라쏼라");
-		System.out.println("시작");
+		System.out.println("종료");
 		System.out.println("용호작업2");
 	}
 
