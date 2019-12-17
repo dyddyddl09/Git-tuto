ca35d093017aab7adb3b1ac27a52733bbe401fb8
mond <esj44518@naver.com>
2019-12-17 18:28:06 +0900

    수정1

--- Git-tuto/src/org/homegit/MainClass.java
+++ Git-tuto/src/org/homegit/MainClass.java
@@ -3,13 +3,13 @@ package org.homegit;
 public class MainClass {
 
 	public static void main(String[] args) {
-		System.out.println("작업자 구용호 쏼라쏼라");
+		System.out.println("작업자 용호 쏼라쏼라");
 		System.out.println("종료");
-		System.out.println("구용호작업2");
+		System.out.println("용호작업2");
 		
 		System.out.println("문동주 시작");
 		System.out.println("문동주 작업");
-		System.out.println("구용호작업3");
+		System.out.println("용호작업3");
 		System.out.println("작업4");
 	}
 
